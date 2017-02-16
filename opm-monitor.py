73da261be6a157d9c9265ecb58b4874af6717913
Anastasia <ana7pana@gmail.com>
2017-02-15 20:58:51 -0500

    Add some data parsing.

--- opm-monitor.py
+++ opm-monitor.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from flask import Flask
 from flask import render_template
@@ -9,7 +10,18 @@ app = Flask(__name__)
 def hello():
     response = requests.get('https://www.opm.gov/json/operatingstatus.json')
 
-    return render_template("hello.html", data=response.json())
+    data = response.json()
+
+    posted_date = data['DateStatusPosted']
+    opm_title = data["Title"]
+    opm_location = data["Location"]
+    opm_status_summary = data["StatusSummary"]
+
+    return render_template("hello.html",
+                           opm_title=opm_title,
+                           opm_location=opm_location,
+                           opm_status_summary=opm_status_summary,
+                           posted_date=posted_date)
 
 if __name__ == "__main__":
     app.run()
