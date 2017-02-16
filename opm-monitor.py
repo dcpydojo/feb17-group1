b51639200180f85f53c86d7e63e4b3fc32713fec
Anastasia <ana7pana@gmail.com>
2017-02-15 20:41:23 -0500

    Add first template and json data

--- opm-monitor.py
+++ opm-monitor.py
@@ -1,9 +1,15 @@
+import requests
+
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    response = requests.get('https://www.opm.gov/json/operatingstatus.json')
+
+    return render_template("hello.html", data=response.json())
 
 if __name__ == "__main__":
     app.run()
